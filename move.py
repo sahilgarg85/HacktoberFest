3f338308089ad384fdee586ccf12cdc5f0f9c1fc
lakshay41 <69715396+lakshay41@users.noreply.github.com>
2020-10-01 23:10:33 +0530

    Create move.py

---
+++ move.py
@@ -0,0 +1,27 @@
+import pygame
+ 
+pygame.init()
+ 
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (255, 0, 0)
+ 
+dis = pygame.display.set_mode((800, 600))
+pygame.display.set_caption('Snake Game by Edureka')
+ 
+game_over = False
+ 
+x1 = 300
+y1 = 300
+ 
+x1_change = 0       
+y1_change = 0
+ 
+clock = pygame.time.Clock()
+ 
+while not game_over:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_
